212b7af2cdfdc399c9f2d1f50b5bac5d813abb4c
jedzelest <jedzelestrelloso@gmail.com>
2024-10-26 20:14:18 +0800

    working on the tablet and phone navigations

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,6 +4,7 @@ interface Props {
   isLoggedIn: boolean;
   navList: {
     id: number;
+    label: string;
     icon: string;
     help_text: string;
   }[];
@@ -13,31 +14,56 @@ const Navigation = ({ navList, isLoggedIn }: Props) => {
   return (
     <>
       {isLoggedIn && (
-        <div className="navigation-container">
-          <nav className="navigation-contents">
-            <img
-              id="positive-space-icon"
-              src=".././public/img/positiveSpace.png"
-              alt="positive space icon"
-            />
-            <ul>
-              {navList.map((nav) => (
-                <li key={nav.id} className="default-menu">
-                  <img src={nav.icon} alt={nav.help_text} />
+        <div>
+          <div className="navigation-container">
+            <nav className="navigation-contents">
+              <img
+                id="positive-space-icon"
+                src=".././public/img/positiveSpace.png"
+                alt="positive space icon"
+              />
+              <ul>
+                {navList.map((nav) => (
+                  <li key={nav.id} className="default-menu">
+                    <img src={nav.icon} alt={nav.help_text} />
+                  </li>
+                ))}
+                <li className="default-menu">
+                  <img
+                    id="user-icon"
+                    src=".././public/img/Black_WhiteJiezel.png"
+                    alt="upload icon"
+                  />
                 </li>
-              ))}
-              <li className="default-menu">
-                <img
-                  id="user-icon"
-                  src=".././public/img/Black_WhiteJiezel.png"
-                  alt="upload icon"
-                />
-              </li>
-              <li className="hamburger-menu">
-                <img src=".././public/img/hamburger.png" alt="hamburger menu" />
-              </li>
-            </ul>
-          </nav>
+                <li className="hamburger-menu">
+                  <img
+                    src=".././public/img/hamburger.png"
+                    alt="hamburger menu"
+                  />
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="responsive-menu">
+            <nav className="responsive-nav">
+              <ul>
+                {navList.map((nav) => (
+                  <li key={nav.id}>
+                    <img src={nav.icon} alt={nav.help_text} />
+                    <h3>{nav.label}</h3>
+                  </li>
+                ))}
+                <li>
+                  <img
+                    id="user-icon"
+                    src=".././public/img/Black_WhiteJiezel.png"
+                    alt="upload icon"
+                  />
+                  <h3>Log-out</h3>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       )}
     </>
--- src/components/Navigation/NavigationData.ts
+++ src/components/Navigation/NavigationData.ts
@@ -1,24 +1,28 @@
 const navigationList = [
   {
     id: 1,
+    label: "Upload Artwork",
     icon: ".././public/img/upload_lite.png",
     help_text: "upload icon",
   },
 
   {
     id: 2,
+    label: "Notifications",
     icon: ".././public/img/notif_bell.png",
     help_text: "notification icon",
   },
 
   {
     id: 3,
+    label: "Messages",
     icon: ".././public/img/sent.png",
     help_text: "message icon",
   },
 
   {
     id: 4,
+    label: "Artwork Market",
     icon: ".././public/img/shopping-cart.png",
     help_text: "shopping cart icon",
   },
